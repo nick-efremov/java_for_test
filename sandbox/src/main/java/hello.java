f32fa9331fd148476b7255ef11c870bcfbc2632e
nick-efremov <48561901+nick-efremov@users.noreply.github.com>
2019-04-09 12:06:18 +0300

    08.04

--- sandbox/src/main/java/hello.java
+++ sandbox/src/main/java/hello.java
@@ -1,5 +1,8 @@
 public class hello {
     public static void main(String[] args)
 
-{System.out.println("Hello, world!");}
+{System.out.println("Hello, world!");
+    System.out.println(2*2);
+    System.out.println(1.0/2);
+}
 }
